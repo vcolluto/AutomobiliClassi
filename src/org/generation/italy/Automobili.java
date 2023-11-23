8ed66129d46a55964a9d11baf20c6d18894fda71
SaraSgarlata <sara-04@live.it>
2023-11-23 09:53:26 +0100

    in elaborazione

---
+++ src/org/generation/italy/Automobili.java
@@ -0,0 +1,19 @@
+package org.generation.italy;
+
+public class Automobili {
+
+	int velocità;
+	float carburante, consumoMedio, kmPercorsi;
+	
+	public void accelera() {
+		
+		if (velocità<=190)
+			velocità+=10; // mi incrementa di 10
+		
+		
+	public void 
+		
+	}
+	
+	
+}
---
+++ src/org/generation/italy/Main.java
@@ -0,0 +1,15 @@
+package org.generation.italy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+		
+		
+	}
+	
+	
+	}

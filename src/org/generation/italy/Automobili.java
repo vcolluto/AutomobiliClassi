3ac58d90b766cddb3cda1e3d98050ec7b49ecf6f
SaraSgarlata <sara-04@live.it>
2023-11-23 09:57:22 +0100

    aggiornato

--- src/org/generation/italy/Automobili.java
+++ src/org/generation/italy/Automobili.java
@@ -3,17 +3,40 @@ package org.generation.italy;
 public class Automobili {
 
 	int velocit‡;
-	float carburante, consumoMedio, kmPercorsi;
+	float carburante, consumoMedio, kmPercorsi; //disponibilit‡Carburante consumoMedio kmPercorsi
 	
 	public void accelera() {
 		
 		if (velocit‡<=190)
 			velocit‡+=10; // mi incrementa di 10
+	}
 		
+	public void decelera() {
 		
-	public void 
+		if (velocit‡>=10)
+			velocit‡-=10; // mi decrementa di 10
+	
+	}
+			
+	public void visualizzaDati() {
 		
+		System.out.println(" velocit‡ attuale :" + velocit‡ + "km/h");
+		System.out.println("carburante: " + carburante + "l");
+		System.out.println("consumo medio: " + consumoMedio + "km/l" );
+		System.out.println("km percorsi: " + kmPercorsi + "km");
 	}
 	
+	public void aggiorna() {
+		
+		kmPercorsi+=velocit‡;
+		carburante-=kmPercorsi/consumoMedio;
 	
 }
+
+	}		
+			
+		
+	
+	
+	
+

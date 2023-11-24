9f6c0fdd0428779b360d7aef8d1ddc81050f48d8
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-11-24 11:39:18 +0100

    aggiunto costruttore

--- src/org/generation/italy/Automobile.java
+++ src/org/generation/italy/Automobile.java
@@ -2,21 +2,55 @@ package org.generation.italy;
 
 public class Automobile {
 
-	int velocit‡;
-	float carburante, consumoMedio, totKmPercorsi; //disponibilit‡Carburante consumoMedio kmPercorsi
+	//private: gli attributi posso esser "visti" solo dall'interno della stessa classe (automobile)
+	private int velocit‡;			
+	private float carburante, consumoMedio, totKmPercorsi; //disponibilit‡Carburante consumoMedio kmPercorsi
 	
-	public void accelera() {
+	//metodo costruttore: si utilizza per inizializzare gli attributi di un oggetto
+	//si differenzia dagli altri metodi perchÈ:
+	// - il nome del metodo Ë uguale al nome della classe
+	// - non ha un valore di ritorno (per il momento: non ha il "void")
+	//ogni volta che si crea un oggetto Automobile bisogna indicare il valore del carburante 
+	//e il valore del consumo medio (nella classe in cui si crea l'oggetto (ad. es. Main))
+	//
+	//le variabili (float carburante, float consumoMedio) indicate nel costruttore 
+	//vengono chiamate "parametri"
+	//e rappresentano i valori che ci vengono forniti da chi "crea" l'oggetto
+	//normalmente i parametri del costruttore hanno lo stesso nome degli attributi.
+	//per capire a quale delle due variabili ci si riferisce, si usa la parola chiave "this".
+	//ad esempio: 
+	//   "this.carburante" 	indica l'attributo della classe
+	//	 "carburante" 		indica il parametro
+	//la parola "this" Ë obbligatoria solo se c'Ë ambiguit‡ (variabili con lo stesso nome).
+	//se i parametri hanno un nome diverso (anche se non Ë la prassi), il "this" non Ë necessario
+	public Automobile(float carburante, float consumoMedio) {
+		if (carburante<0) {
+			System.out.println("carburante non valido");
+			carburante=0;		//valore di default
+		}
+		else
+			this.carburante=carburante;
 		
+		if (consumoMedio<=0) {
+			System.out.println("consumo medio non valido");
+			consumoMedio=10;		//valore di default
+		}
+		else
+			this.consumoMedio=consumoMedio;	//this.consumoMedio Ë l'attributo; consumoMedio Ë il parametro
+			
+		velocit‡=0;
+		totKmPercorsi=0;
+	}
+	
+	
+	public void accelera() {		
 		if (velocit‡<=190 && carburante>0)
-			velocit‡+=10; // mi incrementa di 10
-		
+			velocit‡+=10; // mi incrementa di 10		
 	}
 		
-	public void decelera() {
-		
+	public void decelera() {		
 		if (velocit‡>=10)
-			velocit‡-=10; // mi decrementa di 10
-	
+			velocit‡-=10; // mi decrementa di 10	
 	}
 			
 	public void visualizzaDati() {
@@ -36,9 +70,9 @@ public class Automobile {
 		
 		if(carburante<=0)//non c'Ë carburante la macchina si ferma
 			velocit‡=0;
-}
+	}
 
-	}		
+}		
 			
 		
 	
--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -8,66 +8,70 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
-		Automobile auto1 = new Automobile(); //dichiaro gli oggetti della classe automobile
-		Automobile auto2 = new Automobile();
-		Automobile autoCorrente=auto1;
+		
+		
+		// dichiaro gli oggetti della classe automobile	
+		// ogni volta che si scrive "new Automobile(...)" si esegue il metodo costruttore
+		Automobile auto1 = new Automobile(30,20); 	//crea una nuova automobile con carburante=30 e consumo=20
+		Automobile auto2 = new Automobile(50,23);   //crea una nuova automobile con carburante=50 e consumo=23
+		Automobile autoCorrente = auto1;
 		String risposta;
+
+		/* non lo posso più fare perché gli attributi sono "private"
+		auto1.velocità = 0; // inizializziamo gli attributi, che sono delle variabili
+		auto1.carburante = 30;
+		auto1.consumoMedio = 20;
+		auto1.totKmPercorsi = 0;
+
+		auto2.velocità = 0;
+		auto2.carburante = 50;
+		auto2.consumoMedio = 23;
+		auto2.totKmPercorsi = 0;
+		*/
 		
-		auto1.velocità=0;     //inizializziamo gli attributi, che sono delle variabili
-		auto1.carburante=30;
-		auto1.consumoMedio=20;
-		auto1.totKmPercorsi=0;
-		
-		auto2.velocità=0;
-		auto2.carburante=50;
-		auto2.consumoMedio=23;
-		auto2.totKmPercorsi=0;
-		
+
 		do {
-		
-		System.out.println("\n\n\n\n-Auto1: " );
-		auto1.visualizzaDati();
-		
-		System.out.println("\n-Auto2: ");
-		auto2.visualizzaDati();
-		
-		System.out.println("\nQuale auto vuoi guidare? (1/2/nessuna)");
-		risposta=sc.nextLine();
-		if(risposta.equals("1"))
-			autoCorrente=auto1;
-		else if	(risposta.equals("2"))
-			autoCorrente=auto2;
-		else if	(risposta.equals("nessuna"));
-		
-		else System.out.println("risposta non valida");
-		
-		if(risposta.equals("1") || risposta.equals("2")) {
-			
-		System.out.println("Vuoi accelerare, decelerare?");
-		risposta=sc.nextLine();
-		if(risposta.equals("accelerare"))
-			autoCorrente.accelera();
-		else if (risposta.equals("decelerare"))
-			autoCorrente.decelera();
-		
-		}	
-		auto2.aggiorna();
-		auto1.aggiorna();
-		
-		
-		System.out.println("vuoi continuare?(s/n)");
-		risposta=sc.nextLine();
-		
-		}while(risposta.equals("s"));
-			
-			
-		
-				
-				
-		
-		
-		
+
+			System.out.println("\n\n\n\n-Auto1: ");
+			auto1.visualizzaDati();
+
+			System.out.println("\n-Auto2: ");
+			auto2.visualizzaDati();
+
+			System.out.println("\nQuale auto vuoi guidare? (1/2/nessuna)");
+			risposta = sc.nextLine();
+			if (risposta.equals("1"))
+				autoCorrente = auto1;
+			else if (risposta.equals("2"))
+				autoCorrente = auto2;
+			else if (risposta.equals("nessuna"))
+				;
+
+			else
+				System.out.println("risposta non valida");
+
+			if (risposta.equals("1") || risposta.equals("2")) {
+
+				System.out.println("Vuoi accelerare, decelerare?");
+				risposta = sc.nextLine();
+				if (risposta.equals("accelerare"))
+					autoCorrente.accelera();
+				else if (risposta.equals("decelerare"))
+					autoCorrente.decelera();
+
+			}
+			auto2.aggiorna();
+			auto1.aggiorna();
+
+			System.out.println("vuoi continuare?(s/n)");
+			risposta = sc.nextLine();
+			//auto1.velocità=-20;		//è sbagliato!!!
+			//auto1.consumoMedio=0;       //è sbagliato!!!
+
+		} while (risposta.equals("s"));
+
+		sc.close();
+
 	}
-	
-	
-	}
+
+}
